f16eb53d5e7d8b70f18ab5f1d030b98c411c189b
PRSarte <51978034+PRSarte@users.noreply.github.com>
2019-11-10 18:11:37 +0800

    Add files via upload

---
+++ HW1_1.py
@@ -0,0 +1,7 @@
+ch = str(input('Please enter character to be checked: '))
+
+if (ch == 'A' or ch == 'a' or ch == 'E' or ch == 'e' or ch == 'I' or ch == 'i' 
+or ch == 'O' or ch == 'o' or ch == 'U' or ch == 'u'): 
+    print ('The character ', ch ,' is a vowel')
+else:
+    print ('The character ', ch, ' is a consonant')
